0dd09a3939d0c12889ddd53088787a17a4e05b34
athuljain <athuljainkj2@gmail.com>
2026-02-26 08:16:51 +0530

    landing page update

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,3 @@
-// export default function Home() {
-//   return (
-//     <div style={{ textAlign: "center", marginTop: "100px" }}>
-//       <h1>🍔 Recipe Sharing App</h1>
-//       <p>Share and discover amazing recipes</p>
-
-//       <div style={{ marginTop: "30px" }}>
-//         <a href="/register">
-//           <button>Register</button>
-//         </a>
-
-//         <a href="/login" style={{ marginLeft: "10px" }}>
-//           <button>User Login</button>
-//         </a>
-
-//         <a href="/admin-login" style={{ marginLeft: "10px" }}>
-//           <button>Admin Login</button>
-//         </a>
-//       </div>
-//     </div>
-//   );
-// }
-
 
 "use client";
 import Link from "next/link";
@@ -93,3 +70,6 @@ export default function Home() {
     </div>
   );
 }
+
+
+
